0c3b343884c072c27fecc0ae5ed27381c9a0f837
Samer William <samer.w.alhaddadin@gmail.com>
2020-11-18 00:08:17 +0200

    trying sth... but fail

--- compose.cpp
+++ compose.cpp
@@ -1,22 +1,20 @@
 #include "headers.hpp"
 
-
-
 int main()
 {
     auto data01 = Compose<int, int, int>{}(
         [](auto number) { return number + number; },
         [](auto number) { return number * number; },
-        [] { return 1.5; }());
+        [] { return 1; }());
 
     std::cout << data01 << '\n';
 
     std::cout << [=] { return data01 * 3; }() << '\n';
 
     auto data02 = Compose<float, float, int>{}(
-        [](auto number) { return number / 2.0f; },
-        [](auto number) { return static_cast<float>(number * number); },
-        [] { return -3; }());
+        [](auto number) { return number / 3.59f; },
+        [](auto number) { return static_cast<float>(number * number * number); },
+        [] { return -1; }());
 
     std::cout << data02 << '\n';
 
