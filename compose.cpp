8503d70a4614acdb4de59ec7bf70ed79e9d4df34
Samer William <samer.w.alhaddadin@gmail.com>
2020-11-17 23:58:35 +0200

    More Lambdas

--- compose.cpp
+++ compose.cpp
@@ -4,16 +4,21 @@
 
 int main()
 {
-     
+    auto data01 = Compose<int, int, int>{}(
+        [](auto number) { return number + number; },
+        [](auto number) { return number * number; },
+        [] { return 1.5; }());
 
-     std::cout << Compose<int, int, int>{}(
-                    [](auto number) { return number + number; },
-                    [](auto number) { return number * number; },
-                    3)
-                << '\n';
+    std::cout << data01 << '\n';
 
-     std::cout << Compose<float, float, int>{}(
-         [](auto number) { return number / 2.0f; },
-         [](auto number) { return static_cast<float>(number * number); },
-         3);
+    std::cout << [=] { return data01 * 3; }() << '\n';
+
+    auto data02 = Compose<float, float, int>{}(
+        [](auto number) { return number / 2.0f; },
+        [](auto number) { return static_cast<float>(number * number); },
+        [] { return -3; }());
+
+    std::cout << data02 << '\n';
+
+    std::cout << [=] { return data02 * 3; }() << '\n';
 }
