49f06a0cdc42ce2d48e551a6fc5fc512d8879d88
Samer William <samer.w.alhaddadin@gmail.com>
2020-11-17 20:30:51 +0200

    downgrade tp C++17

--- headers.hpp
+++ headers.hpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <functional>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 //Functor Foo!!
--- vector_tutorial.cpp
+++ vector_tutorial.cpp
@@ -3,38 +3,25 @@
 int main()
 {
     std::vector<int> numbers{1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    // for Algorithm lib
+    // <Algorithm>
     std::for_each(std::begin(numbers),
                   std::end(numbers),
                   [](int const number) -> void {
-                      std::cout << number << ',';
+                      std::cout << number << ' ';
                   });
-
     std::sort(std::begin(numbers),
               std::end(numbers),
               std::greater<int>{});
-
     // NEW LINE
     std::cout << "\n\n";
-
     std::for_each(std::begin(numbers),
                   std::end(numbers),
                   [](int const number) -> void {
-                      std::cout << number << ',';
+                      std::cout << number << ' ';
                   });
-
     // NEW LINE
     std::cout << "\n\n";
-
-    // Sum!!
-    int sum{};
-    std::transform(std::begin(numbers),
-                   std::end(numbers),
-                   &sum,
-                   [&](int acc) -> int {
-                       return sum += acc;
-                   });
-
+    // Sum!! <numic>
+    auto sum = std::accumulate(std::begin(numbers), std::end(numbers), 0.0);
     std::cout << sum << '\n';
 }
